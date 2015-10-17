ec1e6adbd22fcf41951e9ba37e0f0bfd98bcb0b8
Daniel Buchner <danieljb2@gmail.com>
2015-10-17 08:42:22 -0700

    retooled boundary calcs, fixed Chrome issue, works great in Edge

--- src/main.js
+++ src/main.js
@@ -5,39 +5,43 @@
     node.setAttribute('x-splitter-dragging', '');
 
     var props = getProps(node);
-    var lastCoord = event[props.page];
+    var lastCoord = event[props.page] - node[props.edge];
     var next = splitter.nextElementSibling, next = !next.hasAttribute('splitter') && next;
     var previous = splitter.previousElementSibling, previous = !previous.hasAttribute('splitter') && previous;
+    var startingTotal = next[props.offset] + previous[props.offset];
 
     setPercents(node, props);
 
     node.xtag.drag = xtag.addEvent(node, 'move', function(e){
-      var coord = e[props.page];
+      var delta = e[props.page] - node[props.edge] - lastCoord;
+      var nextOffset = next[props.offset];
+      var prevOffset = previous[props.offset];
+      var nextSize = nextOffset - delta;
+      var prevSize = prevOffset + delta;
 
-        var delta = coord > lastCoord ? coord - lastCoord : coord - lastCoord;
-
-        if (coord >= previous[props.edge] && coord <= next[props.edge] + next[props.offset]) {
-          var previousSize = previous[props.offset] + delta;
-          var nextSize = next[props.offset] - delta;
-          
-          if (previousSize > 0 && nextSize > 0) {
-            if (previous) setMinMax(previous, props, previousSize);
-            if (next) setMinMax(next, props, nextSize);
-            lastCoord = coord;
+      if (delta > 0) {
+        if (nextOffset > 0) {
+          if (nextSize <= 0 || prevSize >= startingTotal) {
+            prevSize = startingTotal;
+            nextSize = 0;
           }
-          else {
-            var combine = next[props.offset] + previous[props.offset];
-            if (previousSize <= 0){
-              if (next) setMinMax(next, props, combine);
-              if (previous) setMinMax(previous, props, 0);
-            }
-            else if (nextSize <= 0){
-              if (previous) setMinMax(previous, props, combine);
-              if (next) setMinMax(next, props, 0);
-            }
-            else lastCoord = coord;
-          }
+          setMinMax(next, props, nextSize);
+          setMinMax(previous, props, prevSize);
         }
+      }
+
+      else if (delta < 0) {
+        if (prevOffset > 0) {
+          if (nextSize <= 0 || nextSize >= startingTotal) {
+            nextSize = startingTotal;
+            prevSize = 0;
+          }
+          setMinMax(next, props, nextSize);
+          setMinMax(previous, props, prevSize);
+        }
+      }
+
+      lastCoord = e[props.page] - node[props.edge];
     });
   }
 
@@ -47,15 +51,13 @@
       offset: 'offsetHeight',
       edge: 'offsetTop',
       auto: { max: 'maxWidth', min: 'minWidth' },
-      style: { max: 'maxHeight', min: 'minHeight' },
-      bounds: { start: node.offsetTop, end: node.offsetTop + node.offsetHeight }
+      style: { max: 'maxHeight', min: 'minHeight' }
     } : {
       page: 'pageX',
       offset: 'offsetWidth',
       edge: 'offsetLeft',
       auto: { max: 'maxHeight', min: 'minHeight' },
-      style: { max: 'maxWidth', min: 'minWidth' },
-      bounds: { start: node.offsetLeft, end: node.offsetLeft + node.offsetWidth }
+      style: { max: 'maxWidth', min: 'minWidth' }
     };
     props.parentSize = node[props.offset];
     return props;
